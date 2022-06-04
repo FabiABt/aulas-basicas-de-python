b81662e367fba9f5250b24dfd520c6082290b869
FabiABt <fabiane489@gmail.com>
2022-06-04 20:14:27 -0300

    adicionei comentario

--- aula08d.py
+++ aula08d.py
@@ -1,4 +1,6 @@
+# crie um codigo que calcule a hipotenusa
 co = int(input("Qual o valor do cateto oposto?"))
 ca = int(input(" Qual o valor do cateto adjacent? "))
 print("A hipotenusa vale : {}".format(pow (ca,2) + pow (co,2)))
+# outra fornma de resolucao 
 print("A hipotenusa vale : {}".format(co**2 + ca**2))
