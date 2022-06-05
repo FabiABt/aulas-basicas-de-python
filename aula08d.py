d426ed98051b5d35f624cf27313736c88e8bd74f
FabiABt <fabiane489@gmail.com>
2022-06-05 12:39:45 -0300

    correcao da formula

--- aula08d.py
+++ aula08d.py
@@ -1,6 +1,7 @@
 # crie um codigo que calcule a hipotenusa
-co = int(input("Qual o valor do cateto oposto?"))
-ca = int(input(" Qual o valor do cateto adjacent? "))
-print("A hipotenusa vale : {}".format(pow (ca,2) + pow (co,2)))
+import math
+co = float(input("Qual o valor do cateto oposto?"))
+ca = float(input(" Qual o valor do cateto adjacent? "))
+print("A hipotenusa vale : {}".format(((pow (ca,2) + pow (co,2))**(1/2))))
 # outra fornma de resolucao 
-print("A hipotenusa vale : {}".format(co**2 + ca**2))
+print("A hipotenusa vale : {}".format((co**2 + ca**2)**(1/2)))
