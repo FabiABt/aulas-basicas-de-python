c9933d6a6915f38910be278f1d2c64f191dfe7f0
FabiABt <fabiane489@gmail.com>
2022-06-05 00:46:20 -0300

    novo

--- aula08c.py
+++ aula08c.py
@@ -1,5 +1,5 @@
 from math import trunc
 num = float(input("Digite um termo real: "))
-print(" o numero {} tem a parte inteira {}.".format(num,int(num)))
-print (" o numero {} tem a parte inteira {}.".format(num, trunc(num)))
+print(" o numero {} e sua a parte inteira {}.".format(num,int(num)))
+print (" o numero {} e sua parte inteira {}.".format(num, trunc(num)))
 # tem as duas formar de resolucao sendo chamando o trunc e atrubuindo  inteiro na parte num digitado 
