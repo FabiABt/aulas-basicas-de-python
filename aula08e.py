6baa291fbefd0511e4c80e197efc9fe23b4b756e
FabiABt <fabiane489@gmail.com>
2022-06-05 13:39:03 -0300

    codigo mais limpo

--- aula08e.py
+++ aula08e.py
@@ -1,9 +1,9 @@
 # faca um porograma que leia um angulo qualquer e mostre na tela o valor do sen0, cosseno e tangente desse angulo.
-import math
+from math import radians, sin, cos, tan
 angulo = float(input("Digito o valor do angulo que voce deseja: "))
-seno = math.sin(math.radians(angulo))
+seno = sin(radians(angulo))
 print("O angulo de {} tem o Seno de {:.2f}".format(angulo, seno))
-cos = math.cos(math.radians(angulo))
+cos = cos(radians(angulo))
 print("O angulo de {} tem o Cosseno de {:.2f}".format(angulo, cos))
-tang =  math.tan(math.radians(angulo))
+tang = tan(radians(angulo))
 print("O angulo de {} tem a tangente de {:.2f}".format(angulo, tang))
