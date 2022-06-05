bda948d917de12f1579de3cba0f0b60cfc2cab9f
FabiABt <fabiane489@gmail.com>
2022-06-05 16:55:58 -0300

    arrumada

--- aula08f.py
+++ aula08f.py
@@ -1,7 +1,11 @@
+#sorteando uma orddem na lista 
 import random
-p1 = input(" Digite o nome da 1 pessoa: "),
-p2 = input(" Digite o nome da 2 pessoa: "),
-p3 = input(" Digite o nome da 3 pessoa: "),
-p4 = input(" Digite o nome da 4 pessoa: "),
+p1 = input(" Digite o nome da 1 pessoa: ")
+p2 = input(" Digite o nome da 2 pessoa: ")
+p3 = input(" Digite o nome da 3 pessoa: ")
+p4 = input(" Digite o nome da 4 pessoa: ")
 lista = [p1, p2, p3, p4]
-print(" Os alunos sorteados sao: {}".format(random.choice(lista)))
+escolhido = random.choice(lista)
+print("O aluno sorteado e: {}".format(escolhido))
+#ou 
+print(" O aluno sorteados e: {}".format(random.choice(lista)))
