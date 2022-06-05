7238dbebbc95f8b9be757f068511cc408289fb72
FabiABt <fabiane489@gmail.com>
2022-06-05 19:20:38 -0300

    add shuffle

--- aula08f.py
+++ aula08f.py
@@ -1,11 +1,10 @@
 #sorteando uma orddem na lista 
-import random
+from random import shuffle 
 p1 = input(" Digite o nome da 1 pessoa: ")
 p2 = input(" Digite o nome da 2 pessoa: ")
 p3 = input(" Digite o nome da 3 pessoa: ")
 p4 = input(" Digite o nome da 4 pessoa: ")
 lista = [p1, p2, p3, p4]
-escolhido = random.choice(lista)
-print("O aluno sorteado e: {}".format(escolhido))
-#ou 
-print(" O aluno sorteados e: {}".format(random.choice(lista)))
+shuffle(lista)
+print("A ordem de apresentacao sera: ")
+print(lista)
