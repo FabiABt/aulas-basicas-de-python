4ee357eee462d020c2dd245fcaa563dd9d548028
FabiABt <fabiane489@gmail.com>
2022-06-05 19:44:23 -0300

    .

--- aula08g.py
+++ aula08g.py
@@ -1,9 +1,9 @@
 # Um professor quer sortear um dos seus quatro alunmos para apagar o quadro. Faca um programa que ajude ele, lendo o nome deles e escrevendo o nome do escolhido
-import random
-p1 = str(input(" Digite o nome da 1 pessoa: ")),
-p2 = str(input(" Digite o nome da 2 pessoa: ")),
-p3 = str(input(" Digite o nome da 3 pessoa: ")),
-p4 = str(input(" Digite o nome da 4 pessoa: ")),
+from random import random
+p1 = (input(" Digite o nome da 1 pessoa: ")),
+p2 = (input(" Digite o nome da 2 pessoa: ")),
+p3 = (input(" Digite o nome da 3 pessoa: ")),
+p4 = (input(" Digite o nome da 4 pessoa: ")),
 lista = [p1, p2, p3, p4]
 escolhido = random.choise(lista)
 print("O aluno sorteado e {}".format(escolhido))
