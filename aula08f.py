d3622b8fb1383c69a7f77c79d37a7d35f7a3d81f
FabiABt <fabiane489@gmail.com>
2022-06-05 19:42:30 -0300

    ultima alteracao

--- aula08f.py
+++ aula08f.py
@@ -1,10 +1,11 @@
-#sorteando uma orddem na lista 
-from random import shuffle 
+#sorteando um item na lista 
+from random import choice 
 p1 = input(" Digite o nome da 1 pessoa: ")
 p2 = input(" Digite o nome da 2 pessoa: ")
 p3 = input(" Digite o nome da 3 pessoa: ")
 p4 = input(" Digite o nome da 4 pessoa: ")
 lista = [p1, p2, p3, p4]
-shuffle(lista)
-print("A ordem de apresentacao sera: ")
-print(lista)
+escolhido = choice(lista)
+print(" O aluno sorteado e {}".format(escolhido))
+#ou 
+print(" O aluno sorteados e: {}".format(choice(lista)))
