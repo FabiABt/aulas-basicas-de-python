795db64ac2dea0c89be0cb3a0f5f66a0eb15d767
FabiABt <fabiane489@gmail.com>
2022-06-05 22:48:58 -0300

    retirada de comentario

--- aula08i.py
+++ aula08i.py
@@ -2,7 +2,7 @@
 import pygame
 import time
 pygame.mixer.init()
-pygame.mixer.music.load('./Dream_On.mp3') #D:\projeto\aulas-basicas-de-python
+pygame.mixer.music.load('./Dream_On.mp3')
 pygame.mixer.music.set_volume(90)
 pygame.mixer.music.play()
 while(pygame.mixer.music.get_busy()):
