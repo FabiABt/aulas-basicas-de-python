690e8b95ac612be4f5f6043c55a71fa7b5fea406
FabiABt <fabiane489@gmail.com>
2022-06-09 21:24:18 -0300

    .

---
+++ funcoes.py
@@ -0,0 +1,9 @@
+#funções são blocos de código que realizam determinadas tarefas que normalmente precisam ser executadas diversas vezes dentro de uma aplicação
+def hello(maria):
+    if 123==4:
+        print('Ola',maria)
+    print('Ola',maria)
+    print('Ola',maria)
+    print('Ola',maria)
+    print('Ola',maria)
+print()
