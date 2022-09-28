3faa4ed5fd5139cf2335e5b205bb65e356762781
FabiABt <fabiane489@gmail.com>
2022-09-28 18:03:36 -0300

    atualização

--- aula09adesafio01.py
+++ aula09adesafio01.py
@@ -3,7 +3,7 @@
 # b) o nome com todas as letras em caixa baixa 
 # c) quantas letras tem o nome sem considerar os pepaços
 # d) quantas letras tem o primeiro nome 
-nome = str(input(" Digite seu nome completo: "))
+nome = str(input(" Digite seu nome completo: ")).strip()
 print("Analisando seu nome...")
 print("O seu nome é : {}".format(nome))
 #a
@@ -12,5 +12,9 @@ print("O seu nome em maiusculo é: {}".format(nome.upper()))
 print("O seu nome minusculo é: {}".format(nome.lower()))
 #c
 print("Seu nome tem: {} letras".format(len(nome.replace(" ", ""))))
+# forma resolvida pelo professor foi a descrita abaixo: 
+print("Seu nome tem: {} letas".format(len(nome)- nome.count(" ")))
 #d
 print("O seu primeiro nome tem: {} letras".format(len(nome.split()[0])))
+#forma resolvida pelo professor :
+print("Seu primeiro nome tem {} letras".format(nome.find(" ")))
