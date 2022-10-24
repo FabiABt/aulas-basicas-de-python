ad6791c21e267311b9af7ae4fe18bd72da76ba81
FabiABt <fabiane489@gmail.com>
2022-10-24 10:09:04 -0300

    .

--- aula09adesafio02.py
+++ aula09adesafio02.py
@@ -1,15 +1,12 @@
 # Faça um programa que leia um numero de 0 a 9999 e mostre na tela cada um dos digitos 
 # ex digite um numero : 1834 ----- unidade: 4-- dezena 3. --- centena 8, ---- unidade de milhar 1.--
-num = str(input("Digite um numero de zero a 9999: "))
-if num == None:
-    print("Fim do programa")
-else:
-    for i in range(len(num)-1,0,-1):
-        if i == (len(num)-1):
-            print("{} - unidade".format(num[i]))
-        elif i == (len(num)-2):
-            print("{} - dezena".format(num[i]))
-        elif i == len(num)-3:
-            print("{} - centena".format(num[i]))
-        elif i == len(num)-4:
-            print("{} - Und. milhar".format(num[i]))
+nume = int(input("Digite um numero de zero a 9999: "))
+u = nume // 1 % 10
+d = nume // 10 % 10
+c = nume // 100 % 10
+m = nume // 1000 % 10 
+print(" Analisando o numero {}".format(nume))
+print(" Unidade: {}".format(u))
+print(" Dezena: {}".format(d))
+print(" Centena: {}".format(c))
+print(" Unidade de milhar: {}".format(m))
---
+++ varaiveis.py
@@ -0,0 +1,4 @@
+### Para podermos usar acentuação no Python, devemos definir a codificação utf-8, na versão 3 do Python, isso 
+# ## não é necessário.
+nome = "joão"
+print(nome[0])
