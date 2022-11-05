8135cfe016ff3d9b8e328e713b806a9c5bd6d667
FabiABt <fabiane489@gmail.com>
2022-11-05 15:39:17 -0300

    add de novas atividades

--- variáveis/condicional/adulto_jovem.py
+++ variáveis/condicional/adulto_jovem.py
@@ -1,5 +1,7 @@
 idade= int(input("digite sua idade: "))
-if idade < 20: 
+if idade <= 12:
+    print("Você é criança")
+elif idade < 20: 
     print("Você é jovem")
 elif 20 <= idade < 60:
     print("Você é Adulto ")
