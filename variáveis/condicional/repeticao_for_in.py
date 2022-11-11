d00c4bbbd31d2c99369dfe7211572da1c933e948
FabiABt <fabiane489@gmail.com>
2022-11-11 17:46:22 -0300

    novo arquivo

---
+++ variáveis/condicional/repeticao_for_in.py
@@ -0,0 +1,4 @@
+s= 0
+for i in range(1, 10):
+    print(s)
+    s = s + i
---
+++ variáveis/condicional/teste.py
@@ -0,0 +1,2 @@
+sangue = ((input("Você houve presença de sangue nas fezes? Digite S para sim e N para não: ")).upper)
+print(sangue)
